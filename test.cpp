757f42b5b1e1402baab9a10040420eeda56ecae7
shivankatta@microsoft.com <shivankatta@microsoft.com>
2026-02-13 23:33:31 +0530

    check

---
+++ test.cpp
@@ -0,0 +1,22 @@
+// C++ program to take the input of
+// two numbers and prints its sum
+#include <bits/stdc++.h>
+using namespace std;
+
+// Driver Code
+int main()
+{
+    int a, b;
+
+    // Input two numbers
+    cin >> a >> b;
+
+    // Find the sum
+    int sum = a + b;
+
+    // Print the sum
+    cout << sum;
+
+    return 0;
+}
+
